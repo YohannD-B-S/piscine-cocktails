2edbbc7065aa1b770bc1d1918860458c18ea3956
Yohann Dubuis <yohann.dubuis@lapiscine.pro>
2025-04-30 11:46:03 +0200

    mise ajour

--- src/Controller/CocktailController.php
+++ src/Controller/CocktailController.php
@@ -11,8 +11,9 @@ class CocktailController  extends AbstractController{
       
 
     #[Route('/categories', name: 'list-categories')]
-    public function listCatgories(){
-        $categorieRepository = new CocktailCategoriesRepository;
+    public function listCatgories(CocktailCategoriesRepository $categorieRepository){
+        // en inserant ainsi les parametres on crée une intsance de la classe CocktailCategoriesRepository
+        // et on l'injecte dans la méthode listCategories
         $categories = $categorieRepository->findCategories();
 
         return $this->render('list_categories.html.twig', [
@@ -24,8 +25,12 @@ class CocktailController  extends AbstractController{
     }
 
     #[Route('/categories/{id}', name: 'category-details')]
-    public function categoryDetails($id){
-        $categorieRepository = new CocktailCategoriesRepository;
+    public function categoryDetails(CocktailCategoriesRepository $categorieRepository, $id){
+        // en inserant ainsi les parametres on crée une intsance de la classe CocktailCategoriesRepository
+        //ça nous évite de faire un "new" 
+        // et on l'injecte dans la méthode listCategories
+        // on ajout $id pour récupérer l'id de la catégorie
+       
         $categories = $categorieRepository->findCategoryById($id);
 
         return $this->render('category_details.html.twig',[
@@ -36,8 +41,7 @@ class CocktailController  extends AbstractController{
 
 
     #[Route('/', name: 'home')]
-    public function lastCocktail(){
-        $cocktailsRepository = new CocktailsRepository;
+    public function lastCocktail(CocktailsRepository $cocktailsRepository){
         $cocktails = $cocktailsRepository->findAll();
 
         $lastCocktails = array_slice($cocktails, -2, 2, true); // Récupérer le dernier cocktail de la liste
@@ -49,9 +53,8 @@ class CocktailController  extends AbstractController{
 
     
     #[Route('/cocktails', name : 'list_cocktails')]
-    public function listCocktails(){
+    public function listCocktails(CocktailsRepository $cocktailsRepository){
 
-        $cocktailsRepository = new CocktailsRepository;
         $cocktails = $cocktailsRepository->findAll(); // Récupérer tous les cocktails
     
         return $this->render('list_cocktails.html.twig', [
@@ -66,9 +69,8 @@ class CocktailController  extends AbstractController{
     #[Route('/best_cocktails/{id}', name: 'best_cocktails')]
     // Le paramètre {id} est dynamique et sera passé à la méthode BestCocktail
     // Le nom de la route est 'best_cocktails'
-public function BestCocktail($id){
+public function BestCocktail(CocktailsRepository $cocktailsRepository, $id){
     
-    $cocktailsRepository = new CocktailsRepository;
     $cocktail = $cocktailsRepository->findOneById($id); // Récupérer tous les cocktails
 
     return $this->render('best_cocktail.html.twig', [
